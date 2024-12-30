27d3ee0d46a3c821819294d6c6bc104c650f8bc0
Fred <6550969+MagicFred888@users.noreply.github.com>
2024-12-30 11:26:18 +0100

    Base for 2016

---
+++ AdventOfCode2016/Solver/DayXX.cs
@@ -0,0 +1,22 @@
+﻿namespace AdventOfCode2016.Solver
+{
+    internal partial class DayXX : BaseSolver
+    {
+        public override string PuzzleTitle { get; } = "XXXXXXXXXXXXXXXXX";
+
+        public override string GetSolution1(bool isChallenge)
+        {
+            return "Part 1";
+        }
+
+        public override string GetSolution2(bool isChallenge)
+        {
+            return "Part 1";
+        }
+
+        private void ExtractData()
+        {
+            // Add logic to extract data from challengeData
+        }
+    }
+}
